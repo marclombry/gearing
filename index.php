41e387b4fbef16cca2604c77eb0eef147dd2bded
spikevargas <marc.lombry@gmail.com>
2018-10-13 14:50:34 +0200

    crated a mini framework

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require "vendor/autoload.php";
+use \core\app\classes\Form;
+use \core\app\classes\FormFilter;
+use \core\app\classes\Html;
+use \core\app\classes\Route;
+echo Form::input("text","f");
